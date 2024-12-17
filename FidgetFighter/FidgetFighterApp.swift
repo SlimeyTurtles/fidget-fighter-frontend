ad84c906d7588967ce357c72edc6f7cadb065ad1
Avinh Huynh <avinhahuynh@gmail.com>
2024-12-16 22:37:34 -0800

    Initial Commit

---
+++ FidgetFighter/FidgetFighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetFighterApp.swift
+//  FidgetFighter
+//
+//  Created by Avinh Huynh on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FidgetFighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
